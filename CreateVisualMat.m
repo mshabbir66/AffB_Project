2e86c63343167225c165b3a3c787dff7fc154193
mshabbir66 <mshabbir66@gmail.com>
2014-11-17 13:38:19 +0200

    s
    s

--- CreateVisualMat.m
+++ CreateVisualMat.m
@@ -3,28 +3,30 @@ close all
 clear all
 
 load Dataset/AffectBurstsSession123Cleaned
+load antiAffectBursts
+Samples = [AffectBursts;antiAffectBursts'];
 Vfs=120;
 
-visseq(length(AffectBursts)).data = [];
-for i = 1:length(AffectBursts)
-    fidv = fopen(['../Session',AffectBursts(i).fileName(5),'/dialog/MOCAP_rotated/',AffectBursts(i).fileName,'.txt'],'r');
-    startFrame = round(Vfs*(AffectBursts(i).startTime)/1000);
+visseq(length(Samples)).data = [];
+for i = 1:length(Samples)
+    fidv = fopen(['../Session',Samples(i).fileName(5),'/dialog/MOCAP_rotated/',Samples(i).fileName,'.txt'],'r');
+    startFrame = round(Vfs*(Samples(i).startTime)/1000);
     if startFrame < 1
         startFrame = 1;
     end
-    endFrame = round(Vfs*AffectBursts(i).endTime/1000);
+    endFrame = round(Vfs*Samples(i).endTime/1000);
     
     [~]=textscan(fidv,'%d %f %s',startFrame,'Delimiter','\n','Headerlines',2);
     visseq(i).data=textscan(fidv,'%d %f %s',endFrame-startFrame,'Delimiter','\n');
     fclose(fidv);
     
-%     [y,fs] = wavread(['..\Session',AffectBursts(i).fileName(5),'\dialog\wav\',AffectBursts(i).fileName,'.wav']);
-%     startFrame = round(fs*(AffectBursts(i).startTime)/1000);
+%     [y,fs] = wavread(['..\Session',Samples(i).fileName(5),'\dialog\wav\',Samples(i).fileName,'.wav']);
+%     startFrame = round(fs*(Samples(i).startTime)/1000);
 %     if startFrame < 1
 %         startFrame = 1;
 %     end
-%     endFrame = round(fs*AffectBursts(i).endTime/1000);
-%     switch str2num(AffectBursts(i).fileName(5))
+%     endFrame = round(fs*Samples(i).endTime/1000);
+%     switch str2num(Samples(i).fileName(5))
 %         case 1
 %             y = y(startFrame:endFrame,1);
 %         case 2
