e4731d7abed003e88b3ea79aeacf3fbc65de055e
bbt89 <berkerturker@gmail.com>
2014-12-09 12:57:14 +0200

    b

--- subplotsConfMat.m
+++ subplotsConfMat.m
@@ -18,14 +18,20 @@ close all
 %     axis equal;axis tight;
 % end
 
-matNames={ 'DetectionAudio.mat' ; ... 
-           'DetectionVideo.mat' ; ...
-           'DetectionFused.mat' ; ...
-           'DetectionDecFused.mat'};
+matNames={ 'CascadeRecognitionAudio.mat' ; ... 
+           'CascadeRecognitionVideo.mat' ; ...
+           'CascadeRecognitionFused.mat' ; ...
+           'CascadeRecognitionDecisionFusion.mat' };
        
 figure('Position',[50 50 1000 300]);
 for i=1:4
     load(['EXPproper/' matNames{i}],'ConfusionMatrix');
+    ConfusionMatrixtemp=ConfusionMatrix;
+    ConfusionMatrix=[];
+    ConfusionMatrix(1,1)=sum(sum(ConfusionMatrixtemp(1:2,1:2)));
+    ConfusionMatrix(2,1)=sum(ConfusionMatrixtemp(3,1:2));
+    ConfusionMatrix(1,2)=sum(ConfusionMatrixtemp(1:2,3));
+    ConfusionMatrix(2,2)=ConfusionMatrixtemp(3,3);
     subplot(1,4,i);
     plotConfusionMat;title(['Accuracy: ' num2str(100*sum(diag(ConfusionMatrix))/sum(sum(ConfusionMatrix)),'%.2f') '%']);
     axis equal;axis tight;
