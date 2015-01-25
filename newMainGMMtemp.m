aeeaf08d63d876ab611ad143be8eb614fdee9568
bbt89 <berkerturker@gmail.com>
2015-01-25 17:08:54 +0200

    b

--- newMainGMMtemp.m
+++ newMainGMMtemp.m
@@ -19,7 +19,7 @@ nfold = 10;
 classifierType=2;
 
 % audio 1, video 2, feature fusion 3
-modality=1;
+modality=2;
 
 
 if(classifierType==1)
@@ -70,7 +70,7 @@ IDs=unique(extractfield(AffectDataSync,'id'));
 len=length(IDs);
 load rand_ind.mat%rand_ind = randperm(len);
 rand_id = IDs(rand_ind);
-for i=1:nfold % nfold test
+parfor i=1:nfold % nfold test
   train_ind=[];test_ind=[];
   test_id=rand_id([floor((i-1)*len/nfold)+1:floor(i*len/nfold)]');
   train_id = rand_id;
