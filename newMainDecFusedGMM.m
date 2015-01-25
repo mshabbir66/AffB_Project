68e27490d5b297d155055d721b2910340dd3a407
bbt89 <berkerturker@gmail.com>
2015-01-25 22:59:38 +0200

    b

--- newMainDecFusedGMM.m
+++ newMainDecFusedGMM.m
@@ -57,7 +57,7 @@ IDs=unique(extractfield(AffectDataSync,'id'));
 len=length(IDs);
 load rand_ind.mat%rand_ind = randperm(len);
 rand_id = IDs(rand_ind);
-prob=[];prob3d=[];
+
 for i=1:nfold % nfold test
   train_ind=[];test_ind=[];
   test_id=rand_id([floor((i-1)*len/nfold)+1:floor(i*len/nfold)]');
